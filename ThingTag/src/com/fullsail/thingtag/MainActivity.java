6132dc9ffd6f5a87533214c219db5aa631850b1d
William Saults <wsaults@email.itt-tech.edu>
2013-08-13 22:02:12 -0500

    Trying to fix camera save issue.

--- ThingTag/src/com/fullsail/thingtag/MainActivity.java
+++ ThingTag/src/com/fullsail/thingtag/MainActivity.java
@@ -42,11 +42,14 @@ public class MainActivity extends Activity {
 		cameraButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				// Take a picture and return control to the calling application
-				Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-				fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
-				intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
-				startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+				// create Intent to take a picture and return control to the calling application
+			    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+			    fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
+			    intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+
+			    // start the image capture Intent
+			    startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
 			}
 		});
 
